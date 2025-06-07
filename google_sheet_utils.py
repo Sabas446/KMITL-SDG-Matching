169c901d686d75c151f57d52d793660d6e62a5e8
Sabas446 <sarutibhat.po@kmitl.ac.th>
2025-06-07 20:04:32 +0700

    Update google_sheet_utils.py

--- google_sheet_utils.py
+++ google_sheet_utils.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 import datetime
+from datetime import timezone, timedelta
 import streamlit as st
 
 SHEET_NAME = "sdg_counter"
